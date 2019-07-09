af843a2e173da7e1d963ae2e71e9bdb0890dc895
zhao <zhao1234@qq.com>
2019-07-09 16:47:07 +0800

    配置路由规则

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 // 引入element-ui样式
 import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入路由
+// @表示从src触发之前的路径，不用写index.js，因为默认索引，即：会自动执行index.js
+import router from '@/router'
 // 注册全局
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+// 引入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: []
+})
+export default router
