80772f5627bf4bd10d37c5aa602c2b991e44ea7b
zhao <zhao1234@qq.com>
2019-07-20 19:39:50 +0800

    恢复成个人设置

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,8 @@ import MyBread from './my-bread.vue'
 // 这是测试子传父的
 // import MySon from './my-son.vue'
 // 测试非父子之间传值
-import coma from './comA.vue'
-import comb from './comB.vue'
+// import coma from './comA.vue'
+// import comb from './comB.vue'
 
 // 频道组件
 import MyChannels from './my-channels.vue'
@@ -15,7 +15,7 @@ export default {
     // Vue.component(MySon.name, MySon)
     Vue.component(MyChannels.name, MyChannels)
     Vue.component(MyPublish.name, MyPublish)
-    Vue.component(coma.name, coma)
-    Vue.component(comb.name, comb)
+    // Vue.component(coma.name, coma)
+    // Vue.component(comb.name, comb)
   }
 }
